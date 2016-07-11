15c8a8d52a7914f838eef736a7376d2ff9d233f9
zhangjinwei <3077663517@qq.com>
2016-07-11 15:00:45 +0800

    remove loadding errors.

--- znm/js/c200814.js
+++ znm/js/c200814.js
@@ -964,7 +964,7 @@ function renderText(a, b, d, c, e, f) {
 	ctx.restore()
 }
 var doudouIcon=new Image();
-doudouIcon.src="doudouIcon.png";
+// doudouIcon.src="doudouIcon.png";
 function drawScoreboard() {
 	1 > scoreOpacity && (scoreOpacity += .01, textOpacity += .01);
 	ctx.globalAlpha = textOpacity;
@@ -972,7 +972,7 @@ function drawScoreboard() {
 		b = String(score);
 	6 == b.length ? a = 43 : 7 == b.length ? a = 35 : 8 == b.length ? a = 31 : 9 == b.length && (a = 27);
 	0 === gameState ? (renderText(trueCanvas.width / 2 + gdx + 6 * settings.scale, trueCanvas.height / 2 + gdy, 60, "rgb(236, 240, 241)", String.fromCharCode("0xf04b"), "px FontAwesome"), /*renderText(trueCanvas.width / 2 + gdx + 6 * settings.scale, trueCanvas.height / 2 + gdy - 170 * settings.scale, 150, "#2c3e50", GAME_TEXT.title),*/ renderText(trueCanvas.width /
-		2 + gdx + 5 * settings.scale, trueCanvas.height / 2 + gdy + 100 * settings.scale, 20, "rgb(44,62,80)", GAME_TEXT.start),ctx.drawImage(doudouIcon,0,0,266,120,trueCanvas.width / 2 -120 * settings.scale, trueCanvas.height / 2 +250 * settings.scale,266*settings.scale,120*settings.scale)) : (0 != gameState && 0 < textOpacity && (textOpacity -= .05, renderText(trueCanvas.width / 2 + gdx + 6 * settings.scale, trueCanvas.height / 2 + gdy, 60, "rgb(236, 240, 241)", String.fromCharCode("0xf04b"), "px FontAwesome"), /*renderText(trueCanvas.width / 2 + gdx + 6 * settings.scale, trueCanvas.height / 2 + gdy - 170 * settings.scale, 150, "#2c3e50", GAME_TEXT.title),*/ renderText(trueCanvas.width / 2 + gdx + 5 * settings.scale, trueCanvas.height / 2 + gdy +
+		2 + gdx + 5 * settings.scale, trueCanvas.height / 2 + gdy + 100 * settings.scale, 20, "rgb(44,62,80)", GAME_TEXT.start)/*,ctx.drawImage(doudouIcon,0,0,266,120,trueCanvas.width / 2 -120 * settings.scale, trueCanvas.height / 2 +250 * settings.scale,266*settings.scale,120*settings.scale)*/) : (0 != gameState && 0 < textOpacity && (textOpacity -= .05, renderText(trueCanvas.width / 2 + gdx + 6 * settings.scale, trueCanvas.height / 2 + gdy, 60, "rgb(236, 240, 241)", String.fromCharCode("0xf04b"), "px FontAwesome"), /*renderText(trueCanvas.width / 2 + gdx + 6 * settings.scale, trueCanvas.height / 2 + gdy - 170 * settings.scale, 150, "#2c3e50", GAME_TEXT.title),*/ renderText(trueCanvas.width / 2 + gdx + 5 * settings.scale, trueCanvas.height / 2 + gdy +
 		100 * settings.scale, 20, "rgb(44,62,80)", GAME_TEXT.start)), ctx.globalAlpha = scoreOpacity, renderText(trueCanvas.width / 2 + gdx, trueCanvas.height / 2 + gdy, a, "rgb(236, 240, 241)", score));
 	ctx.globalAlpha = 1
 }
